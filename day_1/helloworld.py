95ac155e64bd513dc0f006cd4dfee5b3a20a3efc
chanatipjuntip303-cell <chanatipjuntip303@gmail.com>
2026-06-28 21:22:49 +0700

    finish day 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,28 @@
+# exercise1&2
+# 2
+print(2 + 3) 
+print(2 - 3)
+print(2 * 3)
+print(2 % 3)
+print(2 / 3)
+print(2 ** 3)
+print(2 // 3)
+# 3
+print('pond')
+print('chanatip')
+print('thailand')
+print('I am enjoying 30 day of python')
+# 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('pond'))
+print(type('chanatip'))
+print(type('thailand'))
+# exercise3
+#1
+print('different between Integer and Float is Float are desimal and Integer are number')
+#2
+print((2-10)**2 + (3-8)**2)
