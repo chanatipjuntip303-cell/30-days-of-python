f0cf5afec1bc27f9c363abfe6f3699bf787b7308
chanatipjuntip303-cell <chanatipjuntip303@gmail.com>
2026-07-01 14:45:49 +0700

    day 4 is full of boring ahh exercise

---
+++ day_4/day-4.py
@@ -0,0 +1,20 @@
+# 1
+challenge = ['Thirt', 'days', 'of', 'python']
+print(' '.join(challenge))
+# 2
+challenge = ['coding', 'for', 'all']
+print(' '.join(challenge))
+#3
+company = "coding for all"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company[7:])
+print(company.find('coding'))
+print(company.replace('coding', 'python'))
+print(company.split())
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(', '))
